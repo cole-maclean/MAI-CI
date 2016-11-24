cb74656667be7b4783ee39a23384205ae03ad489
Cole MacLean <maclean.cole@gmail.com>
2016-11-24 23:34:59 +0100

    --documentation --data-munging finalized data scrapping script, commented code, need to update README

--- Final Project/reddit_scrapper.py
+++ Final Project/reddit_scrapper.py
@@ -10,50 +10,67 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import sys
 
+#Import configuration parameters, user agent for PRAW Reddit object
 config = configparser.ConfigParser()
 config.read('secrets.ini')
 
+#load user agent string
 reddit_user_agent = config.get('reddit', 'user_agent')
 
-def scrape_data(n_subreddits = 10, max_sub_comments = 10):
-    r = praw.Reddit(user_agent=reddit_user_agent)
-    translate_table = dict((ord(char), None) for char in string.punctuation)
-    stop = set(stopwords.words('english'))
-    stemmer = PorterStemmer()
+#main data scrapping script
+def scrape_data(n_submissions = 10, max_sub_comments = 10):
+
+    """This is the main function that runs the scrapping functionality through praw. The random_submission method is called
+    n_submissions times to obtain n random submissions. After receiving a random submission, the .comments method on the submission
+    object is used to get the comments in the submission. The minimum number of comments for all the comments in the submission or max_sub_comments
+    are randomly selected, and the author of each comment is obtained. The comment history of each randomly selected comment author is parsed, and 
+    comment, submission and subreddit data are collected and stored.
+
+    parameters:
+
+    n_submissions - the number of random submissions to select in finding random comments and comment authors to parse their comment history from
+
+    max_sub_comments - the maximum number of comments to randomly select from a random submission for author search and comment history parsing
+
+    """
+    r = praw.Reddit(user_agent=reddit_user_agent) #initialize the praw Reddit object
+    translate_table = dict((ord(char), None) for char in string.punctuation) #dictionary lookup for removing punctuation characters in comment and submission title data
+    stop = set(stopwords.words('english')) #english stop words to filter out non-useful words before storing
+    stemmer = PorterStemmer() #stemming for similiar word aggregation
     with open('reddit_data.json','r') as data_file:    
         reddit_data = json.load(data_file)
     with open('scrapped_users.json','r') as data_file:    
         scrapped_users = json.load(data_file)
-    for i in range(n_subreddits):
+    for i in range(n_submissions):
         print ("scrapping " + str(i) + "th subreddit")
         rand_submission = r.get_random_submission()
         print ("scrapping users from subreddit r/" + rand_submission.subreddit.display_name)
         sub_comments = rand_submission.comments
-        if len(sub_comments) >=3:
-            rnd_comments = random.sample(sub_comments,min(max_sub_comments,len(sub_comments)))
+        if len(sub_comments) >=3: #ensure submission has > 3 comments to filter out submissions with 0 comments, and submissions only containing AutoModerator posts
+            rnd_comments = random.sample(sub_comments,min(len(sub_comments),max_sub_comments)) #select randon number of the min of total number of comments in submission or max_sub_comments
             for comment in rnd_comments:
-                if isinstance(comment, praw.objects.Comment):
+                if isinstance(comment, praw.objects.Comment): #check that return object is a comment object (sometimes a a MoreComment object is returned, which are not currently parsed)
                     user = comment.author
                     if user:
-                        if user.name in scrapped_users:
+                        if user.name in scrapped_users: #check if the users data has already been parsed and skip parsing if True
                             print ('user ' + user.name + 'already scraped')
                         else:
-                            scrapped_users.append(user.name)
+                            scrapped_users.append(user.name) #update already scrapped user cache with currently scraped user
                             for user_comment in user.get_comments(limit=None,_use_oauth=False):
                                 body = user_comment.body.split()
-                                if len(body) >= 10:
-                                    rand_words = random.sample(body,10)
-                                else:
-                                    rand_words = body
+                                #filter out all but the min of 10 words or the total body word count from the comments body to reduce the dataset size.
+                                rand_words = random.sample(body,min(10,len(body)))
                                 clean_comment_words = []
                                 for word in rand_words:
+                                    #perform stop word, punctuation and stemming cleaning on comment body words
                                     if len(word) < 45 and word not in stop:
                                         clean_word = word.translate(translate_table)
                                         wrd_stm = stemmer.stem(clean_word)
                                         clean_comment_words.append(wrd_stm)
+                                #append username, subreddit name, submission title, comment utc timestamp and cleaned random comment body words to dataset
                                 reddit_data.append([user.name,user_comment.subreddit.display_name,user_comment.link_title.split(' '),
                                                                                   user_comment.created_utc,clean_comment_words])
-                        
+    #dump scrapped dataset to script                   
     with open('reddit_data.json','w') as data_file:
         json.dump(reddit_data, data_file)
     with open('scrapped_users.json','w') as data_file:
