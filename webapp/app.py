004b22150b6d77f7a9e233bbfe7e668c8e6e4756
Cole MacLean <maclean.cole@gmail.com>
2017-02-09 20:17:21 +0100

    --bokeh --flask --webdev rescaled bokeh plot, mupdated processing wait text, might be done...

--- webapp/app.py
+++ webapp/app.py
@@ -29,6 +29,7 @@ def index():
     TOOLS=[hover,"crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,"]
 
     p = figure(tools=TOOLS)
+    p.sizing_mode = 'scale_width'
 
     p.scatter("x", "y", radius=0.1, fill_alpha=0.6,
               fill_color="#c7e9b4",
@@ -68,6 +69,7 @@ def recommend():
     TOOLS=[hover,"crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,"]
 
     p = figure(tools=TOOLS)
+    p.sizing_mode = 'scale_width'
 
     p.scatter("x", "y", radius=0.1, fill_alpha=0.6,
               fill_color="colors",
