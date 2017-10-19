67502a19883d6bac02bd3962911476b6138bfd7b
Cole MacLean <maclean.cole@gmail.com>
2017-10-18 18:20:18 -0600

    updated webapp to allow for multi user (cookies)

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,4 @@
-import flask
-from flask import request
+from flask import Flask, request, render_template, make_response
 from Recommender import Recommender
 from BuildRecommender import BuildRecommender
 from bokeh.plotting import figure, show, output_notebook,output_file
@@ -9,15 +8,13 @@ from bokeh.embed import components
 import random
 import networkx as nx
 from networkx.readwrite import json_graph
+import json
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 rec = Recommender()
 
 sc2_rec = BuildRecommender()
-races = ['Terran0','Terran1']
-tree = nx.DiGraph()
-edge_nodes = [1,1]
 
 def race_first_unit(race):
     if race == 'Terran0':
@@ -30,73 +27,77 @@ def race_first_unit(race):
 
 def default_tree(tree,race):
     first_unit = race_first_unit(race)
-    edge_nodes[1] = 1
-    tree.add_node(1,name=first_unit,parent='null')
-    return tree
+    tree.add_node(first_unit,name=first_unit,parent='null')
+    return tree, first_unit
+
+def build_response(tree,races,root):
+    tree_data = json_graph.tree_data(tree,root=root)
+    resp = make_response(render_template('sc2.html',tree_data=tree_data))
+    resp.set_cookie('tree_data', json.dumps(tree_data))
+    resp.set_cookie('races',json.dumps(races))
+    print(json.dumps(tree_data))
+    return resp
 
 @app.route("/sc2")
 def sc2_index():
+    tree = nx.DiGraph()
     default_race = 'Terran'
-    races[0] = default_race + '0'
-    races[1] = default_race + '1'
-    default_tree(tree,races[0])
-    tree_data = json_graph.tree_data(tree,root=1)
-    return flask.render_template("sc2.html",tree_data=tree_data)
+    races = [default_race + '0', default_race + '1']
+    tree, root = default_tree(tree,races[0])
+    resp = build_response(tree,races,root)
+    return resp
 
 @app.route("/sc2_default",methods=['GET','POST'])
 def sc2_default():
-    tree.clear()
-    races[0] = request.form['friendly_race'] + '0'
-    races[1] = request.form['enemy_race'] + '1'
-    default_tree(tree,races[0])
-    tree_data = json_graph.tree_data(tree,root=1)
-    return flask.render_template("sc2.html",tree_data=tree_data)
+    tree= nx.DiGraph()
+    races = [request.form['friendly_race'] + '0',
+             request.form['enemy_race'] + '1']
+    tree,root = default_tree(tree,races[0])
+    resp = build_response(tree,races,root)
+    return resp
 
 @app.route("/sc2_recommend",methods=['GET','POST'])
-def sc2_recommend():   
+def sc2_recommend():
+    tree_data = json.loads(request.cookies.get('tree_data'))
+    print(tree_data)
+    races = json.loads(request.cookies.get('races'))
+    tree = json_graph.tree_graph(tree_data,{'id':'id','children':'children','parent':'parent','name':'name'})
+    root = race_first_unit(races[0])
     if 'autobuild' in request.form.keys():
         autobuild_len = int(request.form["length_autobuild"])
-        node_order = nx.shortest_path(tree,source=edge_nodes[0],target=edge_nodes[1])     
-        build_order = [tree.node[nd]['name'] for nd in node_order]
+        leaf = [n for n,d in tree.out_degree().items() if d==0][0]    
+        build_order = nx.shortest_path(tree,source=root,target=leaf)
         builds = sc2_rec.predict_build(pred_input=build_order,build_length=autobuild_len,races=races)
         for bld in builds:
-            next_node = len(node_order) + 1
-            tree.add_node(next_node,name=bld,parent=node_order[-1])
-            tree.add_edge(node_order[-1],next_node)
-            node_order.append(next_node)
-            edge_nodes[1] = next_node   
+            tree.add_node(bld,name=bld,parent=build_order[-1])
+            tree.add_edge(build_order[-1],bld)
+            build_order.append(bld) 
     else:
         expansion_unit = request.args.get('unit_id')
         custom_build = request.args.get('cust_build')
         if custom_build != 'Custom Build':
             builds = custom_build.split(',')
-            node_order = nx.shortest_path(tree,source=edge_nodes[0],target=edge_nodes[1])
-            build_order = [tree.node[nd]['name'] for nd in node_order]
-            nd_index = build_order.index(expansion_unit)
-            expansion_node = node_order[nd_index]
-            tree.node[expansion_node]["name"] = builds[0]
-            edge_nodes[1] = expansion_node
-            node_order = nx.shortest_path(tree,source=edge_nodes[0],target=edge_nodes[1])            
+            build_order = nx.shortest_path(tree,source=root,target=expansion_unit)[:-1]       
             for nd in tree.nodes():
-                if nd not in node_order:
+                if nd not in build_order:
                     tree.remove_node(nd)
-            for bld in builds[1:]:
-                next_node = len(node_order) + 1
-                tree.add_node(next_node,name=bld,parent=node_order[-1])
-                tree.add_edge(node_order[-1],next_node)
-                node_order.append(next_node)
-                edge_nodes[1] = next_node 
+            for bld in builds:
+                tree.add_node(bld,name=bld,parent=build_order[-1])
+                tree.add_edge(build_order[-1],bld)
+                build_order.append(bld)
         else:
-            node_order = nx.shortest_path(tree,source=edge_nodes[0],target=edge_nodes[1])
-            build_order = [tree.node[nd]['name'] for nd in node_order]
+            expansion_unit = request.args.get('unit_id')
+            build_order = nx.shortest_path(tree,source=root,target=expansion_unit)
+            for nd in tree.nodes():
+                if nd not in build_order:
+                    tree.remove_node(nd)
             next_build = sc2_rec.predict_build(pred_input=build_order,build_length=1,races=races)[-1]
-            next_node = len(node_order) + 1
-            tree.add_node(next_node,name=next_build,parent=node_order[-1])
-            tree.add_edge(node_order[-1],next_node)
-            edge_nodes[1] = next_node
+            tree.add_node(next_build,name=next_build,parent=build_order[-1])
+            tree.add_edge(build_order[-1],next_build)
 
-    tree_data = json_graph.tree_data(tree,root=1)
-    return flask.render_template("sc2.html",tree_data=tree_data)
+    resp = build_response(tree,races,root)
+
+    return resp
 
 @app.route("/")
 def index():   
@@ -124,7 +125,7 @@ def index():
                      source=source)
     p.add_layout(labels)
     script, div = components(p)
-    return flask.render_template("index.html",script=script, div=div)
+    return render_template("index.html",script=script, div=div)
 
 @app.route("/all")
 def all():   
@@ -151,7 +152,7 @@ def all():
                      source=source)
     p.add_layout(labels)
     script, div = components(p)
-    return flask.render_template("all.html",script=script, div=div)
+    return render_template("all.html",script=script, div=div)
 
 @app.route("/recommend")
 def recommend():
@@ -197,7 +198,7 @@ def recommend():
     p.add_layout(labels)
     script, div = components(p)
 
-    return flask.render_template("recommend.html",recommendations = recommendations,script=script, div=div, flag=flag)
+    return render_template("recommend.html",recommendations = recommendations,script=script, div=div, flag=flag)
 
 if __name__ == "__main__":
     import os
